9de2ca59c734744dfa79ad7ede6686338abe3bf0
xgblack <gmg@xgblack.cn>
2019-08-25 21:42:59 +0800

    区间操作-需要修改

---
+++ src/cn/xgblack/leetcode/l0307range_sum_query_mutable/NumArray.java
@@ -0,0 +1,42 @@
+package cn.xgblack.leetcode.l0307range_sum_query_mutable;
+
+/**
+ * @author 小光
+ * @date 2019/8/25 21:08
+ * className: NumArray
+ * description:
+ * ***************************************************************************
+ * Copyright(C),2018-2019,https://blog.xgblack.cn  .All rights reserved.
+ * ***************************************************************************
+ */
+public class NumArray {
+    private int[] sum;
+    private int[] data;
+    public NumArray(int[] nums) {
+        data = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            data[i] = nums[i];
+        }
+        sum = new int[nums.length + 1];
+        for (int i = 1; i < sum.length; i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
+        }
+    }
+
+    public void update(int index, int val) {
+        data[index] = val;
+        for (int i = index + 1; i < sum.length; i++) {
+            sum[i] = sum[i - 1] + data[i - 1];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i];
+    }
+}
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(i,val);
+ * int param_2 = obj.sumRange(i,j);
+ */
