bc35102fea904d1f4965f65f30c156dc314235af
xgblack <gmg@xgblack.cn>
2019-08-27 20:58:20 +0800

    Trie相关问题

---
+++ src/cn/xgblack/leetcode/l0677map_sum_pairs/MapSum.java
@@ -0,0 +1,74 @@
+package cn.xgblack.leetcode.l0677map_sum_pairs;
+
+import java.util.TreeMap;
+
+/**
+ * @author 小光
+ * @date 2019/8/27 14:48
+ * className: MapSum
+ * description:
+ * ***************************************************************************
+ * Copyright(C),2018-2019,https://blog.xgblack.cn  .All rights reserved.
+ * ***************************************************************************
+ */
+class MapSum {
+    private class Node {
+        private int value;
+        public TreeMap<Character, Node> next;
+
+        public Node(int value) {
+            this.value = value;
+            next = new TreeMap<>();
+        }
+        public Node(){
+            this(0);
+        }
+    }
+
+    private Node root;
+    /** Initialize your data structure here. */
+    public MapSum() {
+        root = new Node();
+    }
+
+    public void insert(String word, int val) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        cur.value = val;
+    }
+
+    public int sum(String prefix) {
+        Node cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null) {
+                return 0;
+            }
+            cur = cur.next.get(c);
+        }
+        return sum(cur);
+    }
+
+
+    private int sum(Node node) {
+        int res = node.value;
+        for (char c : node.next.keySet()) {
+            res +=sum(node.next.get(c));
+        }
+
+        return res;
+    }
+}
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum obj = new MapSum();
+ * obj.insert(key,val);
+ * int param_2 = obj.sum(prefix);
+ */
