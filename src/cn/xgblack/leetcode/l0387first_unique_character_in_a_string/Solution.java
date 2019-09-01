84f769494acd20d8912f56df0a31c76dd93c9faa
xgblack <gmg@xgblack.cn>
2019-09-01 23:19:08 +0800

    190901

---
+++ src/cn/xgblack/leetcode/l0387first_unique_character_in_a_string/Solution.java
@@ -0,0 +1,26 @@
+package cn.xgblack.leetcode.l0387first_unique_character_in_a_string;
+
+/**
+ * @author 小光
+ * @date 2019/9/1 17:20
+ * className: Solution
+ * description:
+ * ***************************************************************************
+ * Copyright(C),2018-2019,https://blog.xgblack.cn  .All rights reserved.
+ * ***************************************************************************
+ */
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
