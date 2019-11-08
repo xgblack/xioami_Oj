aa7391d12ca55cc1759aab695b79afd83d408e72
xgblack <gmg@xgblack.cn>
2019-11-08 22:19:15 +0800

    191108

---
+++ src/cn/xgblack/leetcode/l0617merge_two_binary_trees/Solution.java
@@ -0,0 +1,29 @@
+package cn.xgblack.leetcode.l0617merge_two_binary_trees;
+
+/**
+ * @author 小光
+ * @date 2019/11/8 21:51
+ * className: Solution
+ * description:
+ * ***************************************************************************
+ * Copyright(C),2018-2019,https://blog.xgblack.cn  .All rights reserved.
+ * ***************************************************************************
+ */
+public class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return null;
+        }
+        if (t1 == null && t2 != null) {
+            t1 = t2;
+            return t1;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+        t1.val = t1.val + t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+}
---
+++ src/cn/xgblack/leetcode/l0617merge_two_binary_trees/TreeNode.java
@@ -0,0 +1,19 @@
+package cn.xgblack.leetcode.l0617merge_two_binary_trees;
+
+/**
+ * @author 小光
+ * @date 2019/11/8 21:52
+ * className: TreeNode
+ * description:
+ * ***************************************************************************
+ * Copyright(C),2018-2019,https://blog.xgblack.cn  .All rights reserved.
+ * ***************************************************************************
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
