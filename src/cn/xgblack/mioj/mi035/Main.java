82a6816b13d250f010119ca8347a71ff883b10d9
xgblack <gmg@xgblack.cn>
2019-11-17 16:59:23 +0800

    mi035不会

---
+++ src/cn/xgblack/mioj/mi035/Main.java
@@ -0,0 +1,28 @@
+package cn.xgblack.mioj.mi035;
+import java.util.*;
+/**
+ * @author 小光
+ * @date 2019/11/17 16:23
+ * className: Main
+ * description:
+ * ***************************************************************************
+ * Copyright(C),2018-2019,https://blog.xgblack.cn/ All rights reserved.
+ * ***************************************************************************
+ */
+public class Main {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        String line;
+        while (scan.hasNextLine()) {
+            line = scan.nextLine().trim();
+            // please write your code here
+            String[] nums = line.split(",");
+            int m = Integer.parseInt(nums[0]);
+            int n = Integer.parseInt(nums[1]);
+            for (int i = 0; i < m; i++) {
+
+            }
+            // System.out.println("answer");
+        }
+    }
+}
