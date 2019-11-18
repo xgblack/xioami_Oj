76a0b5da1bd9d73bce38e428f3edd3733566e591
xgblack <gmg@xgblack.cn>
2019-11-18 09:28:33 +0800

    题目解决

--- src/cn/xgblack/leetcode/l0942di_string_match/Solution.java
+++ src/cn/xgblack/leetcode/l0942di_string_match/Solution.java
@@ -8,27 +8,15 @@ package cn.xgblack.leetcode.l0942di_string_match;
 public class Solution {
     public int[] diStringMatch(String S) {
         int[] res = new int[S.length() + 1];
-        for (int i = 0; i <= S.length(); i++) {
-            res[i] = i;
-        }
+        int l = 0, r = S.length();
         for (int i = 0; i < S.length(); i++) {
-            if (S.charAt(i) == 'D') {
-                if (res[i] <= res[i + 1]) {
-                    change(res, i, i + 1);
-                }
-            }
-            if (i >= 1 && S.charAt(i) == 'I') {
-                if (res[i] >= res[i + 1]) {
-                    change(res, i - 1, i);
-                }
+            if (S.charAt(i) == 'I') {
+                res[i] = l++;
+            } else {
+                res[i] = r--;
             }
         }
+        res[S.length()] = l;
         return res;
-    }
-
-    private static void change(int[] arr, int a, int b) {
-        int temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
     }
 }
