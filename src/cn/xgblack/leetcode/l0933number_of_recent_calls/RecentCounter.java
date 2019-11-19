423aad2b55585386b402011be886d475b0eff4d1
xgblack <gmg@xgblack.cn>
2019-11-19 09:31:09 +0800

    l0933题目看不懂

---
+++ src/cn/xgblack/leetcode/l0933number_of_recent_calls/RecentCounter.java
@@ -0,0 +1,21 @@
+package cn.xgblack.leetcode.l0933number_of_recent_calls;
+
+/**
+ * @author xgBLACK
+ * @date 2019/11/19 9:19
+ * description:
+ */
+public class RecentCounter {
+    public RecentCounter() {
+
+    }
+
+    public int ping(int t) {
+
+    }
+}
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
---
+++ src/cn/xgblack/leetcode/l0933number_of_recent_calls/Solution.java
@@ -0,0 +1,9 @@
+package cn.xgblack.leetcode.l0933number_of_recent_calls;
+
+/**
+ * @author xgBLACK
+ * @date 2019/11/19 9:19
+ * description:
+ */
+public class Solution {
+}
